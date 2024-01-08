f1c9363a65cbaa8fc404913606377ff35891b72c
rnjsdbswo00 <72205683+rnjsdbswo00@users.noreply.github.com>
2024-01-09 04:00:50 +0900

    third2

---
+++ myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/todo/TodoController.java
@@ -0,0 +1,27 @@
+package com.springboot.myfirstwebapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name") // name을 session에 넣음 이 값을 사용하려는 모든 controller에 넣어줘야함
+public class TodoController {
+
+	public TodoController(TodoService todoService) {
+		super();
+		this.todoService = todoService;
+	}
+
+	private TodoService todoService;
+
+	@RequestMapping("list-todos")
+	public String listAllTodos(ModelMap model) {
+		List<Todo> todos = todoService.findByUsername("in28minutes");
+		model.addAttribute("todos", todos);
+		return "listTodos";
+	}
+}
